088c6ac95b1b1bd640ead6883f9fe437e0bd0a9f
serg <szorin@gmail.com>
2015-01-25 12:34:53 -0500

    solution for hw2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## 'makeCacheMatrix' function creates a matrix that is able to cache its inverse
+## Returns a list of functions to get/set the matrix and it's inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+   inv <- NULL
+   
+   set <- function(m) {
+      x <<- m
+      inv <<- NULL
+   }
+   
+   get <- function() x
+   
+   setinverse <- function(m) inv <<- m
+   
+   getinverse <- function() inv
+   
+   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## 'cacheSolve' return a matrix that is inverse of 'x'. 
+## Use cached result if available
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   m <- x$getinverse()
+   
+   if(is.null(m)) {
+      m <- solve(x$get(), ...)
+      x$setinverse(m)
+   } else {
+      message("getting cached data")
+   }
+   
+   m
 }
